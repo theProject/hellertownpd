3b1e047a4a3c97345c9760e4513cf7b10b1ba1b3
Tristan Smith <tjsmith@bytheproject.com>
2025-06-21 06:53:03 -0400

    all pns inn OG  now

--- app/api/og/route.ts
+++ app/api/og/route.ts
@@ -16,13 +16,13 @@ export async function GET() {
 
   if (hour >= 6 && hour < 12) {
     // Morning opengraph images
-    variants = ["og-morning.JPG", "og-morning-2.JPG", "og-image.png"];
+    variants = ["og-morning.png", "og-image.png"];
   } else if (hour >= 12 && hour < 17) {
     // Afternoon open graph images
-    variants = ["og-afternoon-1.jpg", "og-afternoon.JPG", "og-image-hiring.png"];
+    variants = ["og-afternoon-1.png", "og-afternoon.png", "og-image-hiring.png"];
   } else {
     // Evening & late- night open graph images
-    variants = ["og-evening-1.JPG", "og-evening.jpg"];
+    variants = ["og-evening.png", "og-image.png"];
   }
 
   // Lil algorithm to pick a random one during all three phases of the day
