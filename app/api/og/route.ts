78d909c9065d6e5e027d7aaf82d7cc1dd88ab670
Tristan Smith <tjsmith@bytheproject.com>
2025-06-21 05:17:40 -0400

    go gho og math

--- app/api/og/route.ts
+++ app/api/og/route.ts
@@ -1,3 +1,12 @@
+// This file is part of the Hellertown Police Department website project,
+// in the FUTURE (at owners discretion and post) willl be licensed under the MIT License.  
+// Design and development by theProject. | https://bytheproject.com
+// This code is responsible for dynamically generating Open Graph images based on the time of day.
+// License is unavailable to copy or distribute while in use by the Hellertown Police Department.
+// or until MIT licennse is attached.
+
+// Vercel Edge Function increases performance and reduces latency for dynamic content like Open Graph images - secret sauce
+
 export const runtime = "edge";
 
 export async function GET() {
@@ -6,17 +15,17 @@ export async function GET() {
   let variants: string[] = [];
 
   if (hour >= 6 && hour < 12) {
-    // Morning
+    // Morning opengraph images
     variants = ["og-morning.JPG", "og-morning-2.JPG", "og-image.png"];
   } else if (hour >= 12 && hour < 17) {
-    // Afternoon
+    // Afternoon open graph images
     variants = ["og-afternoon-1.jpg", "og-afternoon.JPG", "og-image-hiring.png"];
   } else {
-    // Evening & night
+    // Evening & late- night open graph images
     variants = ["og-evening-1.JPG", "og-evening.jpg"];
   }
 
-  // Pick a random one
+  // Lil algorithm to pick a random one during all three phases of the day
   const randomImage = variants[Math.floor(Math.random() * variants.length)];
 
   return Response.redirect(`https://hellertownpolice.org${randomImage}`, 302); 
